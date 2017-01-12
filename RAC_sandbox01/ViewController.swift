cf7d7fa8533aa8b9614bd21f07591c4394bdad30
kz56cd <kz56cd@gmail.com>
2017-01-12 18:28:24 +0900

    init project

---
+++ RAC_sandbox01/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RAC_sandbox01
+//
+//  Created by msano on 2017/01/12.
+//  Copyright © 2017年 msano. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.view.hasAmbiguousLayout)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
