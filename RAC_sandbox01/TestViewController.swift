817b6d6af86a3180372e3646385092e58212770c
kz56cd <kz56cd@gmail.com>
2017-01-17 15:21:43 +0900

    Replace testVC

---
+++ RAC_sandbox01/TestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TestViewController.swift
+//  RAC_sandbox01
+//
+//  Created by msano on 2017/01/17.
+//  Copyright © 2017年 msano. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
